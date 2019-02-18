b9f490cf4bf7268697c14e3a66dcd0110b287333
onkarpandit <pandit.onkar9@gmail.com>
2019-02-18 10:38:23 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+Object.defineProperty(exports, "__esModule", {
+    value: true
+})
 
 import { NativeModules } from 'react-native';
+exports.default = NativeModules.SMSRetrieverModule;
 
-const { RNSmsRetriever } = NativeModules;
-
-export default RNSmsRetriever;
